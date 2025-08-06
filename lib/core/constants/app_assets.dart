969cee528773c6a73570ba651824cafecb700af9
Rahim10020 <rahialighi@gmail.com>
2025-08-06 11:11:10 +0000

    ajout des images et centralisation des icones et assets

--- lib/core/constants/app_assets.dart
+++ lib/core/constants/app_assets.dart
@@ -1,3 +1,12 @@
 class AppAssets {
-  //icons
+  static const String greetingImage =
+      'assets/images/illustrations/greeting_image.png';
+  static const String logo1 = 'assets/images/illustrations/logo1.svg';
+  static const String logo2 = 'assets/images/illustrations/logo2.svg';
+  static const String togo = 'assets/images/illustrations/togo.png';
+  static const String france = 'assets/images/illustrations/france.png';
+  static const String maleAvatar =
+      'assets/images/illustrations/male_avatar.png';
+  static const String femaleAvatar =
+      'assets/images/illustrations/female_avatar.png';
 }
---
+++ lib/core/constants/app_icons.dart
@@ -0,0 +1,128 @@
+class AppIcons {
+  // logo
+  static const String icLogo1 = 'assets/images/icons/logo1.svg';
+  static const String icLogo2 = 'assets/images/icons/logo2.svg';
+
+  // Actions d'ajout
+  static const String icAddPhoto = 'assets/images/icons/add_photo.svg';
+  static const String icAddUser = 'assets/images/icons/add_user.svg';
+  static const String icAdd = 'assets/images/icons/add.svg';
+
+  // Navigation et flèches
+  static const String icArrowLeft = 'assets/images/icons/arrow_left.svg';
+  static const String icArrowRight = 'assets/images/icons/arrow_right.svg';
+  static const String icArrowUpLeft = 'assets/images/icons/arrow_up_left.svg';
+  static const String icArrowUpRight = 'assets/images/icons/arrow_up_right.svg';
+  static const String icDoubleArrow = 'assets/images/icons/double_arrow.svg';
+
+  // Interface et formes
+  static const String icCalendar = 'assets/images/icons/calendar.svg';
+  static const String icCircle = 'assets/images/icons/cercle.svg';
+  static const String icClock = 'assets/images/icons/clock.svg';
+  static const String icClose = 'assets/images/icons/close.svg';
+  static const String icCloseCircleFull =
+      'assets/images/icons/close_circle_full.svg';
+
+  // Caméra et média
+  static const String icCamera = 'assets/images/icons/camera.svg';
+  static const String icCameraRotation =
+      'assets/images/icons/camera-rotation.svg';
+  static const String icGallery = 'assets/images/icons/gallery.svg';
+  static const String icImageRotate = 'assets/images/icons/image_rotate.svg';
+  static const String icVideo = 'assets/images/icons/video.svg';
+
+  // Communication
+  static const String icChat = 'assets/images/icons/chat.svg';
+  static const String icChatFull = 'assets/images/icons/chat-full.svg';
+  static const String icFlashMessage = 'assets/images/icons/flashMessage.svg';
+  static const String icPhoneCall = 'assets/images/icons/phone_call.svg';
+  static const String icPhone = 'assets/images/icons/phone.svg';
+  static const String icPhoneFull = 'assets/images/icons/phone-full.svg';
+  static const String icMissCall = 'assets/images/icons/miss_call.svg';
+
+  // Actions et états
+  static const String icCheck = 'assets/images/icons/check.svg';
+  static const String icCheckCircleFull =
+      'assets/images/icons/check-circle-full.svg';
+  static const String icCheckbox = 'assets/images/icons/checkbox.svg';
+  static const String icDoubleCheck = 'assets/images/icons/double_check.svg';
+  static const String icLike = 'assets/images/icons/like.svg';
+  static const String icLikeFull = 'assets/images/icons/like_full.svg';
+  static const String icDislike = 'assets/images/icons/dislike.svg';
+
+  // Fichiers et documents
+  static const String icDocument = 'assets/images/icons/document.svg';
+  static const String icCopy = 'assets/images/icons/copy.svg';
+  static const String icSave = 'assets/images/icons/save.svg';
+  static const String icText = 'assets/images/icons/text.svg';
+
+  // Utilisateurs et profils
+  static const String icUsers = 'assets/images/icons/uisers.svg';
+  static const String icUserCheck = 'assets/images/icons/user_check.svg';
+  static const String icUserFull = 'assets/images/icons/user_full.svg';
+  static const String icUserGroup = 'assets/images/icons/user_group.svg';
+  static const String icStudent = 'assets/images/icons/student.svg';
+  static const String icStudents2 = 'assets/images/icons/students_2.svg';
+  static const String icTeacher = 'assets/images/icons/teacher.svg';
+  static const String icTeacher1 = 'assets/images/icons/teacher_1.svg';
+  static const String icParent = 'assets/images/icons/parent.svg';
+
+  // Divers et utilitaires
+  static const String icCloud = 'assets/images/icons/cloud.svg';
+  static const String icConducteur = 'assets/images/icons/conducteur.svg';
+  static const String icContact = 'assets/images/icons/contact.svg';
+  static const String icDelivery = 'assets/images/icons/delivery.svg';
+  static const String icEnveloppe = 'assets/images/icons/enveloppe.svg';
+  static const String icEye = 'assets/images/icons/eye.svg';
+  static const String icFlash = 'assets/images/icons/flash.svg';
+
+  // Forums et discussions
+  static const String icForum = 'assets/images/icons/forum.svg';
+  static const String icForumFull = 'assets/images/icons/forum_full.svg';
+
+  // Maison et lieux
+  static const String icHouse = 'assets/images/icons/house.svg';
+  static const String icHistorique = 'assets/images/icons/historique.svg';
+
+  // Liens et connexions
+  static const String icLink = 'assets/images/icons/link.svg';
+  static const String icLocalisation = 'assets/images/icons/localisation.svg';
+  static const String icLocalisationCheck =
+      'assets/images/icons/localisation-check.svg';
+  static const String icLock = 'assets/images/icons/lock.svg';
+
+  // Audio et sons
+  static const String icMic = 'assets/images/icons/mic.svg';
+  static const String icMusique = 'assets/images/icons/musique.svg';
+  static const String icSpeakers = 'assets/images/icons/speakers.svg';
+
+  // Notifications et alertes
+  static const String icNotification = 'assets/images/icons/notification.svg';
+
+  // Contrôles de lecture
+  static const String icPause = 'assets/images/icons/pause.svg';
+  static const String icPlay = 'assets/images/icons/play.svg';
+
+  // Outils d'édition
+  static const String icPen = 'assets/images/icons/pen.svg';
+  static const String icPin = 'assets/images/icons/pin.svg';
+
+  // Autres
+  static const String icQuestion = 'assets/images/icons/question.svg';
+  static const String icRotate = 'assets/images/icons/rotate.svg';
+  static const String icSearch = 'assets/images/icons/search.svg';
+  static const String icSend = 'assets/images/icons/send.svg';
+  static const String icSend1 = 'assets/images/icons/send_1.svg';
+  static const String icSetting = 'assets/images/icons/setting.svg';
+  static const String icShare = 'assets/images/icons/share.svg';
+  static const String icStar = 'assets/images/icons/star.svg';
+  static const String icStarFull = 'assets/images/icons/star_full.svg';
+  static const String icSticker = 'assets/images/icons/sticker.svg';
+  static const String icTrash = 'assets/images/icons/trash.svg';
+
+  // Formes et éléments UI
+  static const String icEllipsHorizontal =
+      'assets/images/icons/ellips_horizontal.svg';
+  static const String icEllipsVertical =
+      'assets/images/icons/ellips_vertical.svg';
+}
